c9104718f0841a08bca30ab45cd254c6e408bc36
gunha <wh8299@gmail.com>
2024-12-06 13:30:36 +0900

    add : week4 source code add

---
+++ __Spring_Master/HGH/Code/SpringBootJPA12/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<Category>();
+
+
+}
