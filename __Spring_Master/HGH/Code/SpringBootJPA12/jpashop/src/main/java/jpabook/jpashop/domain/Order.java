7c604e1e8ab2965ee4e1d1220234009544bdd7e2
gunha <wh8299@gmail.com>
2024-12-06 20:08:51 +0900

    add : jpa1-2 code add

--- __Spring_Master/HGH/Code/SpringBootJPA12/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ __Spring_Master/HGH/Code/SpringBootJPA12/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -47,4 +47,44 @@ public class Order {
         orderItem.setOrder(this);
     }
 
+    // == 생성 메서드 === ///
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // === 주문 취소 ===
+    public void cancel(){
+
+        if(this.delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송된 상품은 반환이 불가합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+
+    }
+
+    // == 조회 로직 == //
+
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
---
+++ __Spring_Master/HGH/Code/SpringBootJPA12/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,71 @@
+package jpabook.jpashop.service;
+
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문(){
+        //given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가" , "123-123"));
+        em.persist(member);
+
+        Item book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), 2);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        Assert.assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        Assert.assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+        Assert.assertEquals("주문 가격은 가격 * 수량이다", 20000, getOrder.getTotalPrice());
+
+    }
+
+    @Test
+    public void 주문취소(){
+
+
+    }
+
+    @Test
+    public void 상품주문_재고수량초과(){
+
+    }
+
+}
