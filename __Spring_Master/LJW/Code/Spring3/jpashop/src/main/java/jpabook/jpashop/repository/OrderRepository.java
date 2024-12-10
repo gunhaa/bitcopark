be9aca9117c32b47d6e5d8d6ebf36fc0f89aa3e8
jaewon <jw140@naver.com>
2024-12-10 10:20:26 +0900

    add : springboot와 JPA활용 source code add

--- __Spring_Master/LJW/Code/Spring3/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ __Spring_Master/LJW/Code/Spring3/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -6,6 +6,7 @@ import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -98,5 +99,13 @@ public class OrderRepository {
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);//최대 1000건
 
         return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
     }
 }
