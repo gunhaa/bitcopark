34614574f0f404d3890fed141371c06d29543ebe
gunha <wh8299@gmail.com>
2024-12-10 20:56:53 +0900

    add : jpa2 code add

---
+++ __Spring_Master/HGH/Code/SpringBoot-실전! 스프링 부트와 JPA활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    // 해당 api는 정상적으로 반환되긴 하지만 엔티티가 직접 노출되는 문제가 있다.
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName());
+        }
+
+        return all;
+    }
+
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+
+    static class OrderDto {
+
+    }
+}
