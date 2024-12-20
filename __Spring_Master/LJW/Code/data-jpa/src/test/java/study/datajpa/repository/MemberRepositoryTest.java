8f97138206a8086736d79c63e5c080c197d37a5a
jaewon <jw140@naver.com>
2024-12-20 12:48:14 +0900

    add : update spring data jpa

--- __Spring_Master/LJW/Code/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ __Spring_Master/LJW/Code/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -176,4 +176,25 @@ class MemberRepositoryTest {
     void callCustom() {
         List<Member> result = memberRepository.findMemberCustom();
     }
+
+    @Test
+    void nativeQuery() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
+        }
+    }
 }
