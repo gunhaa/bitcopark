4ef420d47f069456eef9eca024fe706beded2656
gunha <wh8299@gmail.com>
2025-01-10 15:29:57 +0900

    add : querydsl sourcecode update

--- __Spring_Master/HGH/Code/QueryDSL/querydsl/src/main/java/study/querydsl/dto/MemberDto.java
+++ __Spring_Master/HGH/Code/QueryDSL/querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -16,6 +16,7 @@ public class MemberDto {
     // QueryDSL이 알 수 있게하는 어노테이션이다
     // 하지만 이렇게하면 MemberDto가 querydsl의존성을 가지게 되는 단점이있다.
     // ex) querydsl 라이브러리를 빼야할 경우
+    // DTO가 많은 레이어에 흘러다닌다. querydsl 의존성이 있으니 아키텍쳐 적으로 위험 할 수있다.
     @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
